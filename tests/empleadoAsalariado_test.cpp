272b9b13c1c635840a4c2facc04b5ee4c53051d8
Juan Aguilar <Juan.aguilartorres@ucr.ac.cr>
2022-05-29 20:48:22 -0600

    Ajustes de escenarios

--- tests/empleadoAsalariado_test.cpp
+++ tests/empleadoAsalariado_test.cpp
@@ -1,2 +1,85 @@
 #include <gtest/gtest.h>
 #include "../src/empleadoAsalariado.h"
+
+namespace
+{
+    TEST(empleadoAsalariado_Test, Test_Calcular_Pago_Bruto)
+    {
+        /// AAA
+
+        // Arrange - configurar el escenario
+
+        // Act - ejecute la operación
+
+    }
+
+    TEST(empleadoAsalariado_Test, Test_Calcular_Pago_Neto)
+    {
+        /// AAA
+
+        // Arrange - configurar el escenario
+
+        // Act - ejecute la operación
+
+    }
+
+    TEST(empleadoAsalariado_Test, Test_Calcular_Impuestos)
+    {
+        /// AAA
+
+        // Arrange - configurar el escenario
+
+        // Act - ejecute la operación
+
+    }
+
+    TEST(empleadoAsalariado_Test, Test_Obtener_Id)
+    {
+        /// AAA
+
+        // Arrange - configurar el escenario
+
+        // Act - ejecute la operación
+
+    }
+
+    TEST(empleadoAsalariado_Test, Test_Obtener_Supervisor)
+    {
+        /// AAA
+
+        // Arrange - configurar el escenario
+
+        // Act - ejecute la operación
+
+    }
+
+    TEST(empleadoAsalariado_Test, Test_Obtener_Nombre_Completo)
+    {
+        /// AAA
+
+        // Arrange - configurar el escenario
+
+        // Act - ejecute la operación
+
+    }
+
+    TEST(empleadoAsalariado_Test, Test_Insertar_SubEmpleado)
+    {
+        /// AAA
+
+        // Arrange - configurar el escenario
+
+        // Act - ejecute la operación
+
+    }
+
+    TEST(empleadoAsalariado_Test, Test_Obtener_Información_de_Salida)
+    {
+        /// AAA
+
+        // Arrange - configurar el escenario
+
+        // Act - ejecute la operación
+
+    }
+}
---
+++ tests/planilla_test.cpp
@@ -0,0 +1,67 @@
+#include <gtest/gtest.h>
+#include "../src/planilla.h"
+
+namespace
+{
+    TEST(planilla_Test, Test_Agregar_Empleado)
+    {
+        /// AAA
+
+        // Arrange - configurar el escenario
+
+        // Act - ejecute la operación
+
+    }
+
+    TEST(planilla_Test, Test_Obtener_Empleado)
+    {
+        /// AAA
+
+        // Arrange - configurar el escenario
+
+        // Act - ejecute la operación
+
+    }
+
+    TEST(planilla_Test, Test_Obtener_Subtotal)
+    {
+        /// AAA
+
+        // Arrange - configurar el escenario
+
+        // Act - ejecute la operación
+
+    }
+
+    TEST(planilla_Test, Test_Obtener_Impuestos)
+    {
+        /// AAA
+
+        // Arrange - configurar el escenario
+
+        // Act - ejecute la operación
+
+    }
+
+    TEST(planilla_Test, Test_Obtener_Total)
+    {
+        /// AAA
+
+        // Arrange - configurar el escenario
+
+        // Act - ejecute la operación
+
+    }
+
+    TEST(planilla_Test, Test_Operador_de_Salida)
+    {
+        /// AAA
+
+        // Arrange - configurar el escenario
+
+        // Act - ejecute la operación
+
+    }
+
+
+}
