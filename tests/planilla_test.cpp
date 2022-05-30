4d3551754d62264ced0858b163ae4e997ea786ee
Juan Aguilar <Juan.aguilartorres@ucr.ac.cr>
2022-05-29 22:30:21 -0600

    Ajustados casos de planilla.

--- tests/planilla_test.cpp
+++ tests/planilla_test.cpp
@@ -3,34 +3,83 @@
 
 namespace
 {
-    TEST(planilla_Test, Test_Agregar_Empleado)
-    {
-        /// AAA
-
-        // Arrange - configurar el escenario
-
-        // Act - ejecute la operación
-
-    }
-
     TEST(planilla_Test, Test_Obtener_Empleado)
     {
         /// AAA
 
         // Arrange - configurar el escenario
+        string stringPersonas = "1 Juan Aguilar juan_aguilar@biz.com 1 1";
+        istringstream streamPersonas (stringPersonas);
+
+        string stringNomina = "";
+        istringstream streamNomina (stringNomina);
+
+        string stringHoras = "1 250 2";
+        istringstream streamHoras (stringHoras);
+
+        planilla *laPlanilla = new planilla(&streamPersonas, &streamNomina, &streamHoras);
+
 
         // Act - ejecute la operación
+        empleadoPorHoras *empleadoEsperado = new empleadoPorHoras(1, "Juan", "Aguilar", 1, 500);
+        empleado *empleadoActual = laPlanilla->obtenerEmpleado(1);
 
+        delete laPlanilla;
+        // Assert - valide los resultados
+        EXPECT_EQ(empleadoEsperado, empleadoActual);
     }
+
+    TEST(planilla_Test, Test_Agregar_Empleado)
+    {
+        /// AAA
+
+        // Arrange - configurar el escenario
+        string stringPersonas = "";
+        istringstream streamPersonas (stringPersonas);
+
+        string stringNomina = "";
+        istringstream streamNomina (stringNomina);
+
+        string stringHoras = "";
+        istringstream streamHoras (stringHoras);
+
+        planilla *laPlanilla = new planilla(&streamPersonas, &streamNomina, &streamHoras);
+        empleadoPorHoras *empleadoEsperado = new empleadoPorHoras(1, "Juan", "Aguilar", 1, 500);
+
+
+        // Act - ejecute la operación
+        laPlanilla->agregarEmpleado(empleadoEsperado);
+        empleado *empleadoActual = laPlanilla->obtenerEmpleado(1);
+
+        delete laPlanilla;
+        // Assert - valide los resultados
+        EXPECT_EQ(empleadoEsperado, empleadoActual);
+    }
+
 
     TEST(planilla_Test, Test_Obtener_Subtotal)
     {
         /// AAA
 
         // Arrange - configurar el escenario
+        string stringPersonas = "1 Denis Tyler denis_tyler@biz.com 1 1 \n 2 Clarissa Parker clarissa_parker@biz.com 1 1";
+        istringstream streamPersonas (stringPersonas);
+
+        string stringNomina = "1 4500";
+        istringstream streamNomina (stringNomina);
+
+        string stringHoras = "2 40 50";
+        istringstream streamHoras (stringHoras);
+
+        planilla *laPlanilla = new planilla(&streamPersonas, &streamNomina, &streamHoras);
 
         // Act - ejecute la operación
+        float actual = laPlanilla->obtenerSubtotal();
+        float esperado = (4500)*(0.93) + (40)*(50);
 
+        delete laPlanilla;
+        // Assert - valide los resultados
+        EXPECT_FLOAT_EQ(esperado, actual);
     }
 
     TEST(planilla_Test, Test_Obtener_Impuestos)
@@ -38,9 +87,24 @@ namespace
         /// AAA
 
         // Arrange - configurar el escenario
+        string stringPersonas = "1 Denis Tyler denis_tyler@biz.com 1 1 \n 2 Clarissa Parker clarissa_parker@biz.com 1 1";
+        istringstream streamPersonas (stringPersonas);
+
+        string stringNomina = "1 4500";
+        istringstream streamNomina (stringNomina);
+
+        string stringHoras = "2 40 50";
+        istringstream streamHoras (stringHoras);
+
+        planilla *laPlanilla = new planilla(&streamPersonas, &streamNomina, &streamHoras);
 
         // Act - ejecute la operación
+        float actual = laPlanilla->obtenerImpuestos();
+        float esperado = (4500)*(0.07);
 
+        delete laPlanilla;
+        // Assert - valide los resultados
+        EXPECT_FLOAT_EQ(esperado, actual);
     }
 
     TEST(planilla_Test, Test_Obtener_Total)
@@ -48,9 +112,24 @@ namespace
         /// AAA
 
         // Arrange - configurar el escenario
+        string stringPersonas = "1 Denis Tyler denis_tyler@biz.com 1 1 \n 2 Clarissa Parker clarissa_parker@biz.com 1 1";
+        istringstream streamPersonas (stringPersonas);
+
+        string stringNomina = "1 4500";
+        istringstream streamNomina (stringNomina);
+
+        string stringHoras = "2 40 50";
+        istringstream streamHoras (stringHoras);
+
+        planilla *laPlanilla = new planilla(&streamPersonas, &streamNomina, &streamHoras);
 
         // Act - ejecute la operación
+        float actual = laPlanilla->obtenerTotal();
+        float esperado = (4500) + (40)*(50);
 
+        delete laPlanilla;
+        // Assert - valide los resultados
+        EXPECT_FLOAT_EQ(esperado, actual);
     }
 
     TEST(planilla_Test, Test_Operador_de_Salida)
@@ -58,9 +137,24 @@ namespace
         /// AAA
 
         // Arrange - configurar el escenario
+        string stringPersonas = "1 Denis Tyler denis_tyler@biz.com 1 1 \n 2 Clarissa Parker clarissa_parker@biz.com 1 1";
+        istringstream streamPersonas (stringPersonas);
+
+        string stringNomina = "1 4500";
+        istringstream streamNomina (stringNomina);
+
+        string stringHoras = "2 40 50";
+        istringstream streamHoras (stringHoras);
+
+        planilla *laPlanilla = new planilla(&streamPersonas, &streamNomina, &streamHoras);
 
         // Act - ejecute la operación
+        string actual = "a";
+        string esperado = "b";
 
+        delete laPlanilla;
+        // Assert - valide los resultados
+        EXPECT_EQ(esperado, actual);
     }
 
 
